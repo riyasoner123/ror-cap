0f41a5a986079ac8f14027067432f7ce6222bae0
riya <Riya.tout@gmail.com>
2024-03-01 15:45:15 +0530

    some changes

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,16 +1,16 @@
 # config valid for current version and patch releases of Capistrano
 lock "~> 3.18.0"
 
-set :application, 'commodre'
-set :repo_url, 'git@github.com:ohiodn8/commodre.git' # should match git repo
+set :application, 'demo'
+set :repo_url, 'git@github.com:riyasoner123/ror-cap.git' # should match git repo
 set :branch, :master
-set :deploy_to, '/home/deploy/commodre'
+set :deploy_to, '/home/demo/demo'
 set :pty, true
 set :linked_files, %w{config/database.yml config/secrets.yml}
 set :linked_dirs, %w{bin log tmp/pids tmp/cache tmp/sockets vendor/bundle public/system public/uploads}
 set :keep_releases, 5
-set :rvm_type, :user
-set :rvm_ruby_version, 'ruby-2.4.0' # Should match ruby version
+set :rbenv_type, :user
+set :rbenv_ruby, '3.0.3'
 
 # Puma configuration
 set :puma_rackup, -> { File.join(current_path, 'config.ru') }
--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -59,3 +59,4 @@
 #     auth_methods: %w(publickey password)
 #     # password: "please use keys"
 #   }
+server '35.154.46.122', user: 'demo', roles: %w{web app db}
