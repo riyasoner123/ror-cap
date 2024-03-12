7aab051d90e94e1d1a00ae97e338cb73080448a4
riya <riya.tout@gmail.com>
2024-03-12 16:26:08 +0530

    add

--- config/puma.rb
+++ config/puma.rb
@@ -1,53 +1,39 @@
-# frozen_string_literal: true
-
-# Puma can serve each request in a thread from an internal thread pool
-# The `threads` method setting takes two numbers: a minimum and maximum
-# Any libraries that use thread pools should be configured to match
-# the maximum value specified for Puma. The default is set to 5 threads for minimum
-# and maximum; this matches the default thread size of Active Record
-#
 threads_count = ENV.fetch('RAILS_MAX_THREADS') { 5 }
 threads threads_count, threads_count
 
 # Specifies the `environment` that Puma will run in.
-# Defaults to development
-rails_env = ENV.fetch('RAILS_ENV') { 'development' }
+# Defaults to production
+rails_env = ENV.fetch('RAILS_ENV') { 'production' }
 environment rails_env
 
-if %w[production staging].member?(rails_env)
-  app_dir = '/home/ubuntu/demo/shared'
-  directory app_dir
-  shared_dir = "#{app_dir}/tmp"
-  # Logging
-  stdout_redirect "#{app_dir}/log/puma.stdout.log", "#{app_dir}/log/puma.stderr.log", true
+# Set up directory paths
+app_dir = '/home/ubuntu/demo/current'
+shared_dir = '/home/ubuntu/demo/shared'
 
-  # Set master PID and state locations
-  pidfile "#{shared_dir}/pids/puma.pid"
-  state_path "#{shared_dir}/pids/puma.state"
+# Logging
+stdout_redirect "#{shared_dir}/log/puma.stdout.log", "#{shared_dir}/log/puma.stderr.log", true
 
-  # Change to match your CPU core count
-  workers ENV.fetch('WEB_CONCURRENCY') { 2 }
+# Set master PID and state locations
+pidfile "#{shared_dir}/tmp/pids/puma.pid"
+state_path "#{shared_dir}/tmp/pids/puma.state"
 
-  preload_app!
+# Change to match your CPU core count
+workers ENV.fetch('WEB_CONCURRENCY') { 2 }
 
-  # Set up socket location
-  bind "unix://#{shared_dir}/sockets/puma.sock"
+preload_app!
 
-  before_fork do
-    # app does not use database, uncomment when needed
-    # ActiveRecord::Base.connection_pool.disconnect!
-  end
+# Set up socket location
+bind "unix://#{shared_dir}/tmp/sockets/puma.sock"
 
-  on_worker_boot do
-    ActiveSupport.on_load(:active_record) do
-      # app does not use database, uncomment when needed
-      # db_url = ENV.fetch('DATABASE_URL')
-      # puts "puma: connecting to DB at #{db_url}"
-      # ActiveRecord::Base.establish_connection(db_url)
-    end
-  end
-elsif rails_env == 'development'
-  # Specifies the `port` that Puma will listen on to receive requests; default is 3000.
-  port   ENV.fetch('PORT') { 3009 }
-  plugin :tmp_restart
+before_fork do
+  # Disconnect from the database
+  ActiveRecord::Base.connection_pool.disconnect! if defined?(ActiveRecord)
 end
+
+on_worker_boot do
+  # Reconnect to the database
+  ActiveRecord::Base.establish_connection if defined?(ActiveRecord)
+end
+
+
+
